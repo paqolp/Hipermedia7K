e29b1957ae4bf3f7a2fe5867dfdf8e60b588bc71
Jose Angel Torres Martinez <jtorres24@ucol.mx>
2018-08-20 11:31:14 -0500

    Add files via upload

---
+++ Torres Martinez Jose Angel/02. TMJA-Ejercicio/FshVideo/FshVideo.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel=styleSheet HREF="Style/styleletter.css" TYPE="text/css" MEDIA=screen>
+    <title>Fsh Video</title>
+  </head>
+  <body>
+
+  </body>
+</html>
---
+++ Torres Martinez Jose Angel/02. TMJA-Ejercicio/FshVideo/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Fsh Video Login</title>
+    <link rel=StyleSheet HREF="style/login.css" TYPE="text/css" MEDIA=screen>
+
+  </head>
+  <body background="IMG/fondo.jpg">
+    <div class="encabezado">
+      <h1 id="logo">FHS VIDEO</h1>
+      <font class="normalFont" id="lema">"Video makes everuthing posible and everything goes on"</font>
+
+    </div>
+    <div class="login" align="center">
+      <h1>Login</h1><br>
+      <form class="" action="FshVideo.php" method="post" align="center">
+        <label class="normalFont"for="">Usuario</label>
+        <input type="text" name="user" value=""><br>
+        <label class="normalFont"for="">Contrasena</label>
+        <input type="password" name="password" value=""><br>
+        <input type="submit" name="aceptar" value="Aceptar">
+      </form>
+
+    </div>
+  </body>
+</html>
