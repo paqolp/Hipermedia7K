beb8f829b045099781148332adf989ab8287d132
CarmenMiranda <cmiranda@ucol.mx>
2018-09-18 22:40:18 -0500

    Corrección de 3 botones a un botón de Estilo

---
+++ Miranda Gonzalez Carmen Liliana/10. MGCL - Cambios de estilo - 1 boton/js/main.js
@@ -0,0 +1,18 @@
+var estilo=2;
+
+function cambiarEstilo(){
+    switch(estilo){
+        case 1:
+            document.getElementById('hojaEstilo').href="css/style1.css";
+            estilo=2;
+            break;
+        case 2:
+            document.getElementById('hojaEstilo').href="css/style2.css";
+            estilo=3;
+            break;
+        case 3:
+            document.getElementById('hojaEstilo').href="css/style3.css";
+            estilo=1;
+            break;
+    }
+}
